8b56c83e25a6a944b8dead19309c170b31ccfbe4
Jack Peterson <jack@tinybike.net>
2015-07-24 23:11:18 -0700

    Improved formatting; default to bdb instead of src/bdb folder

--- install.sh
+++ install.sh
@@ -4,67 +4,86 @@
 # Syntax:
 #   ./install.sh /path/to/bitcoin
 #
-# If no path is provided, BDB 4.8 installs to src/bdb.
+# If no path is provided, BDB 4.8 is installed locally to a new bdb/ folder.
 #
 # @author Jack Peterson (jack@tinybike.net)
 
 set -e
 trap "exit" INT
 
+BLUE='\033[1;34m'
+RED='\033[1;31m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
 startdir=$(pwd)
 
 if [ -n "$1" ]; then
-    target=$1
+    target=$1/src
 else
     target=$(pwd)
-    mkdir -p src
+    mkdir -p bdb
 fi
 
-echo "Installing Berkeley DB 4.8 to $target/src/bdb..."
-
-cd "$target/src"
+cd "$target"
 
 if [ ! -d "bdb" ]; then
-    if [ ! -f "$startdir/bdb-4.8.tar.gz" ]; then
-        wget -O $startdir/bdb-4.8.tar.gz http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz
+
+    bdbtargz="bdb-4.8.tar.gz"
+
+    if [ ! -f "$startdir/$bdbtargz" ]; then
+
+        bdburl="http://download.oracle.com/berkeley-db/db-4.8.30.NC.tar.gz"
+
+        echo -e "${RED}Downloading $bdburl...${NC}"
+        wget -O $startdir/$bdbtargz $bdburl
+
     fi
+
+    echo -e "${BLUE}Installing Berkeley DB 4.8 to $target/bdb...${NC}"
     mkdir -p bdb
 
-    echo " - unpack bdb-4.8.tar.gz -> src/bdb"
-    tar -xvf $startdir/bdb-4.8.tar.gz -C bdb --strip-components=1 > /dev/null 2>&1
+    echo -e "${CYAN}  - unpack $bdbtargz -> ${PWD##*/}/bdb${NC}"
+    tar -xvf $startdir/$bdbtargz -C bdb --strip-components=1 > /dev/null 2>&1
 
     cd bdb/build_unix
     mkdir -p build
 
     export BDB_PREFIX=$(pwd)/build
 
-    echo " - dist/configure"
+    echo -e "${CYAN}  - dist/configure${NC}"
     ../dist/configure --disable-shared --enable-cxx --with-pic --prefix=$BDB_PREFIX > /dev/null 2>&1
 
-    echo " - install -> src/bdb/build_unix/build"
+    echo -e "${CYAN}  - install -> ${PWD##*/}/bdb/build_unix/build${NC}"
     make install > /dev/null 2>&1
 
     cd ../..
-    echo "Done."
+    echo -e "${RED}Done.${NC}"
+
 else
+
     export BDB_PREFIX=$(pwd)/bdb/build_unix/build
 
-    echo "Found local Berkeley DB: $BDB_PREFIX"
+    echo "${RED}Found local Berkeley DB: $BDB_PREFIX${NC}"
+
 fi
 
 cd "$startdir"
+
 g++ version.cpp -I${BDB_PREFIX}/include/ -L${BDB_PREFIX}/lib/ -o version
 ./version
 
 if [ -f "$target/configure.ac" ] || [ -f "$target/configure.in" ]; then
+
     cd "$target"
 
-    echo "Creating configure script..."
+    echo -e "${BLUE}Creating configure script...${NC}"
     autoreconf --install --force --prepend-include=${BDB_PREFIX}/include/ > /dev/null 2>&1
 
-    echo "Running configure..."
+    echo -e "${BLUE}Configuring...${NC}"
     ./configure CPPFLAGS="-I${BDB_PREFIX}/include/" LDFLAGS="-L${BDB_PREFIX}/lib/" > /dev/null 2>&1
 
     cd "$startdir"
-    echo "Done."
+    echo -e "${RED}Done.${NC}"
+
 fi
--- version.cpp
+++ version.cpp
@@ -4,7 +4,7 @@
 
 int main(int argc, char *argv[])
 {
-    printf("Berkeley DB version: %d.%d\n", DB_VERSION_MAJOR, DB_VERSION_MINOR);
+    printf("Berkeley DB version check: %d.%d\n", DB_VERSION_MAJOR, DB_VERSION_MINOR);
     assert(DB_VERSION_MAJOR == 4 && DB_VERSION_MINOR == 8);
     return 0;
 }
